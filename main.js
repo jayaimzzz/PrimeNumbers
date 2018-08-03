bd396821f881bac769cd2cf791856c8527dfaf95
James Horton <jameshorton@gmail.com>
2018-08-03 11:45:52 -0400

    adjusted how number is written to DOM

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function isThisPrime(a) {
     let counter = 2
     let result = true
@@ -7,11 +8,15 @@ function isThisPrime(a) {
             counter = a
         } else {
             result = true
+            // writeToScreen(counter)
+            // setTimeout(writeToScreen(counter), 1000)
             counter++
         }
     }
     return result
 }
+
+
 
 //This function is not working yet.
 function findTheNextPrime(a) {
@@ -19,18 +24,19 @@ function findTheNextPrime(a) {
     let foundIt = false
     while (foundIt = false){
         let counter = 2
-
+        
     }
     foundIt = true
     let result = a + i
 }
 
+let primeNumbersDiv = document.getElementById("primeNumbersDiv")
 function listThisManyPrimeNumbers(a) {
     let counter = 1
     let primeCounter = 1
     while (counter <= a) {
         if (isThisPrime(primeCounter)) {
-            document.write(primeCounter + ", ")
+            writeToScreen(primeCounter)
             counter++
             primeCounter++
         } else {
@@ -38,5 +44,16 @@ function listThisManyPrimeNumbers(a) {
         }
     }
 }
+
+function writeToScreen(number) {
+    const numberToDisplay = document.createTextNode(number + ", ")
+    primeNumbersDiv.appendChild(numberToDisplay)
+    // primeNumbersDiv.textContent += number + ", "
+
+    // primeNumbersDiv.appendChild(number + ", ")
+    // document.write(number + ", ")
+}
+
+
 let quantityOfPrimeNumbers = prompt("How many prime numbers would you like to calculate?")
 listThisManyPrimeNumbers(quantityOfPrimeNumbers)
